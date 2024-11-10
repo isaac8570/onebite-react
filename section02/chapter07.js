728ebbd867df2f53e2ae8badb7d1cc8fa7caea65
isaac8570 <isaac8570@gachon.ac.kr>
2024-11-10 23:58:47 +0900

    Day 5 분발합시다 ..

--- section02/chapter07.js
+++ section02/chapter07.js
@@ -3,7 +3,7 @@
 //1. push
 // 배열의 맨 뒤에 새로운 요소를 추가하는 메서드
 let arr1 = [1, 2, 3];
-arr1.push(4);
+const newLength = arr1.push(4, 5, 6, 7);
+
 console.log(arr1);
-arr1.push(5, 6, 7);
-console.log(arr1);
+console.log(newLength);
