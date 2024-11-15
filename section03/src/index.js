0962e8f9a6bb4b640828ea0f054c9e1e797b2533
isaac8570 <isaac8570@gachon.ac.kr>
2024-11-16 00:34:30 +0900

    Day 8

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,4 @@
-console.log("hi node.js");
+import mul, { add, sub } from "./math.js";
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(2, 3));
---
+++ section03/src/math.js
@@ -0,0 +1,12 @@
+// math module
+
+export function add(a, b) {
+  return a + b;
+}
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multifly(a, b) {
+  return a * b;
+}
